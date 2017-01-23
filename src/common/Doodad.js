59695d551aaddefa998244131157ccda9e035bf6
Claude Petit <doodadjs@gmail.com>
2017-01-23 06:57:10 -0500

    Check if destroyed before.

--- src/common/Doodad.js
+++ src/common/Doodad.js
@@ -2070,13 +2070,13 @@ module.exports = {
 										} else {
 											try {
 												if (types.isClass(type) && this._implements(mixIns.Events)) {
-													var errorEvent = this.__ERROR_EVENT;
-													if (errorEvent && (attr !== errorEvent)) {
-														var destroyed = false;
-														if (this._implements(mixIns.Creatable)) {
-															destroyed = _shared.getAttribute(this, __Internal__.symbolDestroyed);
-														};
-														if (destroyed === false) { // NOTE: Can be 'null' for "not created".
+													var destroyed = false;
+													if (this._implements(mixIns.Creatable)) {
+														destroyed = _shared.getAttribute(this, __Internal__.symbolDestroyed);
+													};
+													if (destroyed === false) { // NOTE: Can be 'null' for "not created".
+														var errorEvent = this.__ERROR_EVENT;
+														if (errorEvent && (attr !== errorEvent)) {
 															var ev = new doodad.ErrorEvent(ex);
 															this[errorEvent](ev);
 															if (ev.prevent) {
