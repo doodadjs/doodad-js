871545fba20faa72dc2cfa27809468c95950e50a
Claude Petit <doodadjs@gmail.com>
2017-02-21 17:00:45 -0500

    Move "types.toSource" to its own module.

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ module.exports = {
 		
 		DD_MODULES = (DD_MODULES || {});
 		require(sourceDir + "common/Types.js").add(DD_MODULES);
+		require(sourceDir + "common/Types_ToSource.js").add(DD_MODULES);
 		require(sourceDir + "common/Types_Bind.js").add(DD_MODULES);
 		require(sourceDir + "common/Types_Generators.js").add(DD_MODULES);
 		require(sourceDir + "common/Types_Iterators.js").add(DD_MODULES);
