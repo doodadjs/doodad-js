d7f61835a1d66c9f6a1ae9d8af36b26a00a46c3a
Claude Petit <doodadjs@gmail.com>
2017-06-03 03:59:54 -0400

    Add new natives
    [skip ci]

--- src/common/Types_UUIDS.js
+++ src/common/Types_UUIDS.js
@@ -106,6 +106,12 @@ module.exports = {
 						// Edge
 						'DOMTokenList': 'DOMSettableTokenList',
 						'DOMSettableTokenList': 'DOMTokenList',
+						
+						// Firefox vs Edge
+						'RTCDTMFSender': 'RTCDtmfSender',
+						'RTCDtmfSender': 'RTCDTMFSender',
+						'WebKitFileSystem': 'FileSystem',
+						'FileSystem': 'WebKitFileSystem',
 					});
 
 					for (let i = 0; i < tempNatives.length; i++) {
