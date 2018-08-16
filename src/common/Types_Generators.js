fc7cf76ce16375fb272534ec5fd4586a71d4bccb
Claude Petit <doodadjs@gmail.com>
2018-08-16 16:37:11 -0400

    Review of native functions.

--- src/common/Types_Generators.js
+++ src/common/Types_Generators.js
@@ -60,7 +60,7 @@ exports.add = function add(modules) {
 
 			tools.complete(_shared.Natives, {
 				// "isGeneratorFunction" Firefox (why "isGenerator" is in the prototype ???)
-				functionIsGeneratorCall: (types.isFunction(global.Function.prototype.isGenerator) ? global.Function.prototype.isGenerator.call.bind(global.Function.prototype.isGenerator) : null),
+				functionIsGeneratorCall: (types.isFunction(global.Function.prototype.isGenerator) ? _shared.Natives.functionBindCall(global.Function.prototype.isGenerator) : null),
 
 				GeneratorFunction: types.getPrototypeOf(
 					function* () {
