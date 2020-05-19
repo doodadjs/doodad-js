8c32831c1dd46d7873d1cae20647e2420f520607
Claude Petit <doodadjs@gmail.com>
2020-05-19 18:24:31 -0400

    'global' is never there

--- src/make/res/package.templ.js
+++ src/make/res/package.templ.js
@@ -82,4 +82,4 @@
 				return root.Doodad.Namespaces.load(modules, options, startup);
 			});
 	};
-}).call(undefined, ((typeof globalThis === 'object') && (globalThis !== null) ? globalThis : ((typeof global === 'object') && (global !== null) ? global : window)));
+})((typeof globalThis === 'object') && (globalThis !== null) ? globalThis : window);
